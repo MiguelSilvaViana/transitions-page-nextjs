173de89159b62099375c02f62442b38bdae3173b
Miguel Silva <miguelsilvavianaa@gmail.com>
2024-01-16 12:09:02 -0300

    stairs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+/* transitions required */
+import Link from 'next/link';
+import { AnimatePresence } from 'framer-motion';
+
+/* transitions components */
+import Stairs from '@/components/Layout/Stairs';
+import Wave from '@/components/Layout/Wave';
+
+
+export default function App({ Component, pageProps, router }) {
+  return (
+    <div className='main'>
+      <div className="header">
+        <Link href={'/'}>Home</Link>
+        <Link href={'/about'}>About</Link>
+        <Link href={'/contact'}>Contact</Link>
+      </div>
+
+      <AnimatePresence mode='wait'>
+        {/* <Wave /> */}
+        <Stairs />
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
+    </div>
+  );
+
 }
---
+++ src/pages/about/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <main className='main'>
+      <h1>About</h1>
+    </main>
+  );
+};
+
+export default page;
